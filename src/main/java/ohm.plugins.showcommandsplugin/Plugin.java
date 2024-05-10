4d133d01148b700e1ab35d83c3faa9f6aff500e8
Adrien B <bourdeaux.adrien@gmail.com>
2024-05-10 18:16:08 -0400

    Added support for commands run by server

--- src/main/java/ohm.plugins.showcommandsplugin/Plugin.java
+++ src/main/java/ohm.plugins.showcommandsplugin/Plugin.java
@@ -7,6 +7,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
+import org.bukkit.event.server.ServerCommandEvent;
+import org.bukkit.event.server.RemoteServerCommandEvent;
 import java.util.ArrayList;
 import java.util.List;
 public class Plugin extends JavaPlugin implements Listener {
@@ -17,11 +19,24 @@ public class Plugin extends JavaPlugin implements Listener {
   }
 
   @EventHandler
-  public void onCommandSent(PlayerCommandPreprocessEvent event) {
+  public void onPlayerCommandSent(PlayerCommandPreprocessEvent event) {
     List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
     for(int i = 0; i < players.size(); i++){
-      players.get(i).sendMessage(event.getPlayer().getName() + " tried to execute the command " + event.getMessage());
+      players.get(i).sendMessage(event.getPlayer().getName() + " executed the command " + event.getMessage());
     }
   }
-
+  @EventHandler
+  public void onServerCommandSent(ServerCommandEvent event){
+    List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+    for(int i = 0; i < players.size(); i++){
+      players.get(i).sendMessage("Server executed the command " + event.getCommand());
+    }
+  }
+  @EventHandler
+  public void onRemoteServerCommandSent(RemoteServerCommandEvent event){
+    List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+    for(int i = 0; i < players.size(); i++){
+      players.get(i).sendMessage("Server executed the command " + event.getCommand());
+    }
+  }
 }
