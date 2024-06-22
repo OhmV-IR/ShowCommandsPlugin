6a179184789ebb6e77e75fba6dfe558d9723ab22
Adrien B <bourdeaux.adrien@gmail.com>
2024-06-22 16:21:31 -0400

    able to detect if commands are run

---
+++ src/main/java/com/example/examplemod/ShowCommandsMod.java
@@ -0,0 +1,47 @@
+package com.example.examplemod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.CommandEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(ShowCommandsMod.MODID)
+public class ShowCommandsMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "showcommandsmod";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+
+    public ShowCommandsMod()
+    {
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+    @SubscribeEvent
+    public void onCommandReceived(CommandEvent event){
+        LOGGER.info("received command");
+        LOGGER.info(event.toString());
+    }
+}
